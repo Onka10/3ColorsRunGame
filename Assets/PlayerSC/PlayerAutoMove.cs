83a6a05c8b84fd467c2dd931e6d6a7a90fb01da6
onkalo <87790609+Onka10@users.noreply.github.com>
2022-10-29 16:22:04 +0900

    物理演算とかFixUpに変更

--- Assets/PlayerSC/PlayerAutoMove.cs
+++ Assets/PlayerSC/PlayerAutoMove.cs
@@ -9,16 +9,19 @@ namespace Player
     {
         PlayerManager manager;
         float speed;
+        Rigidbody rb;
 
         void Start(){
             manager = PlayerManager.I;
             speed = manager.Data.speed;
+            rb = GetComponent<Rigidbody>();
         }
 
-        void Update()
+        void FixedUpdate()
         {
             if(!manager.IsMove) return;
-            transform.position += new Vector3(speed, 0, 0) * Time.deltaTime;
+            // transform.position += new Vector3(speed, 0, 0) * Time.deltaTime;
+            rb.velocity = new Vector3(speed,rb.velocity.y,rb.velocity.z);
         }
     }
 }
